4ed99a63c29f24e1fd0c24eb004b4ab8fa85c7e2
avshalomp <avshi1987@gmail.com>
2019-01-14 22:33:38 +0200

    refactor UI

--- src/components/Movies/ImageFooter.js
+++ src/components/Movies/ImageFooter.js
@@ -7,11 +7,11 @@ import { IconContext } from 'react-icons';
 const ImageFooter = () => {
     return (
         <div className={styles.imgFooter}>
-            <IconContext.Provider value={{color: '#606060', size: '1.4em'}}>
-                <div className={styles.hoverOpacity}><FaEdit /></div>
+            <IconContext.Provider value={{color: '#26a69a', size: '1.4em'}}>
+                <div className={styles.editIcon}><FaEdit /></div>
             </IconContext.Provider>
-            <IconContext.Provider value={{color: '#ff4832', size: '1.2em'}}>
-                <div className={styles.hoverOpacity}><FaTrashAlt /></div>
+            <IconContext.Provider value={{color: '#E57373', size: '1.2em'}}>
+                <div className={styles.removeIcon}><FaTrashAlt /></div>
             </IconContext.Provider>
         </div>
     )
