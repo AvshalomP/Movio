cb23f3ca7eaf5a2077bbbb8ba1750ce560cbca65
avshalomp <avshi1987@gmail.com>
2019-01-16 17:34:17 +0200

    add API key as env variable

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { config }  from "dotenv";
+
+config(); //config vars - dotenv
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
