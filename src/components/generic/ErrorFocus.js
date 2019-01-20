412d0c9a2e1e0af5c2294722589e523de9410136
avshalomp <avshi1987@gmail.com>
2019-01-20 16:52:54 +0200

    fix ErrorFocus causing crash

--- src/components/generic/ErrorFocus.js
+++ src/components/generic/ErrorFocus.js
@@ -6,7 +6,7 @@ class ErrorFocus extends React.Component {
         const { isSubmitting, isValidating, errors } = prevProps.formik;
         const keys = Object.keys(errors);
         if (keys.length > 0 && isSubmitting && !isValidating) {
-            const selector = `[id="${keys[0]}"]`;
+            const selector = `[name="${keys[0]}"]`;
             const errorElement = document.querySelector(selector);
             errorElement.focus();
         }
